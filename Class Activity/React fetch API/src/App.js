7a8beab7040de7fc288b886969b9dc6a16f00000
Basem <basem.bahattab.bm@gmail.com>
2023-05-21 21:53:37 +0300

    react API

---
+++ Class Activity/React fetch API/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchCity from './SearchCity.js';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Today's Weather</h1>
+      <SearchCity />
+      </header>
+    </div>
+  );
+}
+
